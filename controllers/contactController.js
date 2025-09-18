0fb2faa80cdc26c5fbdbcd350b379a68838643e3
Delta Student <dhakadn317@gmail.com>
2025-09-18 15:18:55 +0530

    feat: create Express Router and contactController for CRUD operations with middleware

---
+++ controllers/contactController.js
@@ -0,0 +1,45 @@
+// give labels for contact form fields
+// @desc Get all contacts
+// @route GET /
+// @access public
+
+const getContacts = (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+};
+
+// @desc Create New contact
+// @route POST /api/contacts
+// @access public
+const createContact = (req, res) => {
+  console.log("The request body id:",req.body);
+  res.status(201).json({ message: "Create Contact" });
+};
+
+// @desc Get contact
+// @route GET /api/contacts/:id
+// @access public
+const getContact = (req, res) => {
+  res.status(200).json({ message: `Get contact fof ${req.params.id}` });
+};
+
+// @desc Update contact
+// @route PUT /api/contacts/:id
+// @access public
+const updateContact = (req, res) => {
+  res.status(201).json({ message: `Update contact for ${req.params.id}` });
+};
+
+// @desc Delete contact
+// @route DELETE /api/contacts/:id
+// @access public
+const deleteContact = (req, res) => {
+  res.status(200).json({ mesasge: `Delete contact for ${req.params.id}` });
+};
+
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
